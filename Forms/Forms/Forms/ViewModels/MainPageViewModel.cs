c4fceeee74694cf0003f292d40d312abe5fbe6da
Kevin Malan <kmalan@outlook.com>
2019-04-06 19:53:02 +0200

    Styled MainPage

--- Forms/Forms/Forms/ViewModels/MainPageViewModel.cs
+++ Forms/Forms/Forms/ViewModels/MainPageViewModel.cs
@@ -1,18 +1,12 @@
 ﻿using Forms.Views;
 using Prism.Commands;
-using Prism.Mvvm;
 using Prism.Navigation;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Forms.ViewModels
 {
     public class MainPageViewModel : ViewModelBase
     {
-        INavigationService _navigationService;
+        private INavigationService _navigationService;
         public DelegateCommand Register { get; set; }
         public DelegateCommand ListAccounts { get; set; }
 
@@ -33,6 +27,5 @@ namespace Forms.ViewModels
         {
             await _navigationService.NavigateAsync(nameof(ListAccountsPage));
         }
-
     }
 }
